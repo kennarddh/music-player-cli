6b27c7e070121dfc123206bc4e0ea1f9ba963eaf
kennarddh <79390992+kennarddh@users.noreply.github.com>
2023-01-20 19:23:39 +0700

    feat: Loop ask

--- src/index.ts
+++ src/index.ts
@@ -5,31 +5,33 @@ import SearchYoutube from './Features/Youtube/SearchYoutube/SearchYoutube.js'
 import NewPlaylist from './Features/NewPlaylist/NewPlaylist.js'
 import SelectPlaylist from './Features/SelectPlaylist/SelectPlaylist.js'
 
-const { action } = await inquirer.prompt({
-	type: 'list',
-	message: 'Select action',
-	name: 'action',
-	choices: [
-		{ value: 'searchYoutube', name: 'Search on Youtube' },
-		{ value: 'newPlaylist', name: 'Create New Playlist' },
-		{ value: 'selectPlaylist', name: 'Select a Playlist' },
-	],
-})
+while (true) {
+	const { action } = await inquirer.prompt({
+		type: 'list',
+		message: 'Select action',
+		name: 'action',
+		choices: [
+			{ value: 'searchYoutube', name: 'Search on Youtube' },
+			{ value: 'newPlaylist', name: 'Create New Playlist' },
+			{ value: 'selectPlaylist', name: 'Select a Playlist' },
+		],
+	})
 
-switch (action) {
-	case 'searchYoutube': {
-		await SearchYoutube()
+	switch (action) {
+		case 'searchYoutube': {
+			await SearchYoutube()
 
-		break
-	}
-	case 'newPlaylist': {
-		await NewPlaylist()
+			break
+		}
+		case 'newPlaylist': {
+			await NewPlaylist()
 
-		break
-	}
-	case 'selectPlaylist': {
-		await SelectPlaylist()
+			break
+		}
+		case 'selectPlaylist': {
+			await SelectPlaylist()
 
-		break
+			break
+		}
 	}
 }
