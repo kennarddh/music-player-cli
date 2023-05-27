8ba8050d84cde9967cc9084c1bd08ceede4ebf08
kennarddh <79390992+kennarddh@users.noreply.github.com>
2023-05-28 02:13:41 +0700

    feat: Show selected playlist in status

--- src/Features/Status/Status.ts
+++ src/Features/Status/Status.ts
@@ -1,11 +1,20 @@
 import inquirer from 'inquirer'
 import SoundEvent from '../../Data/SoundEvent.js'
+import Data from '../../Data/Data.js'
 
 const Status = async () => {
 	console.log(`Is muted: ${SoundEvent.isMuted}`)
 	console.log(`Is looped: ${SoundEvent.isLooped}`)
 	console.log(`Volume: ${SoundEvent.volume * 100}`)
 	console.log(`Status: ${SoundEvent.status.toString()}`)
+
+	const playlistName = (await Data.CheckHaveSelectedPlaylist())
+		? (await Data.data)?.playlists?.[
+				(await Data.data).selectedPlaylist as string
+		  ]?.name as string
+		: 'None'
+
+	console.log(`Selected Playlist: ${playlistName}`)
 
 	while (true) {
 		const { done } = await inquirer.prompt({
