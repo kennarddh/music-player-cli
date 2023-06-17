bd39170dd3eace90968238c928098d58ae2c834b
kennarddh <79390992+kennarddh@users.noreply.github.com>
2023-06-17 13:26:50 +0700

    feat: Delete thumbnail when delete sound

--- src/Features/DeleteSounds/DeleteSounds.ts
+++ src/Features/DeleteSounds/DeleteSounds.ts
@@ -2,14 +2,19 @@ import inquirer from 'inquirer'
 import Data from '../../Data/Data.js'
 import SoundsData from '../../Data/SoundsData.js'
 import SecondsToDuration from '../../Utils/SecondsToDuration.js'
+import ThumbnailsData from '../../Data/ThumbnailsData.js'
 
 const DeleteSounds = async () => {
 	const { selected } = await inquirer.prompt({
 		type: 'checkbox',
 		message: 'Select sounds to delete',
 		name: 'selected',
-		choices: (await Data.GetCurrentPlaylistSounds()).map(result => ({
-			name: `${result.title}, ${result.author}, ${SecondsToDuration(result.duration)}`,
+		choices: (
+			await Data.GetCurrentPlaylistSounds()
+		).map(result => ({
+			name: `${result.title}, ${result.author}, ${SecondsToDuration(
+				result.duration
+			)}`,
 			value: result.id,
 		})),
 		loop: false,
@@ -19,6 +24,7 @@ const DeleteSounds = async () => {
 		await Data.DeleteSound(id)
 
 		await SoundsData.Delete(id)
+		await ThumbnailsData.Delete(id)
 	}
 }
 
