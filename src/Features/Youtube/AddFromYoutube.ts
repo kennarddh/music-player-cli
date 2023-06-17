d29daadb103d6779ceb218ef58e089d78642772f
kennarddh <79390992+kennarddh@users.noreply.github.com>
2023-06-17 13:25:35 +0700

    feat: Add from youtube download thumbnail

--- src/Features/Youtube/AddFromYoutube.ts
+++ src/Features/Youtube/AddFromYoutube.ts
@@ -1,38 +1,66 @@
 import ora from 'ora'
 import ytdl from 'ytdl-core'
+import sharp from 'sharp'
+
 import SoundsData from '../../Data/SoundsData.js'
 import YoutubeUrlFromId from '../../Utils/Youtube/YoutubeUrlFromId.js'
+import ThumbnailsData from '../../Data/ThumbnailsData.js'
+
+declare const fetch: Function
 
 const AddFromYoutube = (id: string, youtubeId: string, title: string) =>
 	new Promise<void>((resolve, reject) => {
 		const url = YoutubeUrlFromId(youtubeId)
 
-		const audio = ytdl(url, { quality: 'highestaudio' })
-
-		const loader = ora(`Downloading ${title}, 0.00%`)
+		const loader = ora(`Downloading ${title} thumbail`)
 
 		loader.start()
 
-		audio.on('progress', (_, downloaded, total) => {
-			loader.text = `Downloading ${title}, ${(
-				(downloaded / total) *
-				100
-			).toFixed(2)}%`
-		})
+		ytdl.getInfo(url)
+			.then(info => {
+				const thumbnailUrl = info.videoDetails.thumbnails.at(-1)
+					?.url as string // Every video has a thumbnail
 
-		SoundsData.Add(id, audio).then(writeStream => {
-			writeStream.once('finish', () => {
-				loader.succeed(`Downloaded ${title}`)
+				return fetch(thumbnailUrl).then((response: any) => {
+					if (!response.ok) reject(response.status)
 
-				resolve()
+					return response.arrayBuffer() as Buffer
+				})
 			})
+			.then((thumbnailBuffer: Buffer) =>
+				ThumbnailsData.Add(id, thumbnailBuffer)
+			)
+			.then(() => {
+				const audio = ytdl(url, { quality: 'highestaudio' })
 
-			writeStream.once('error', () => {
+				loader.text = `Downloading ${title} audio, 0.00%`
+
+				audio.on('progress', (_, downloaded, total) => {
+					loader.text = `Downloading ${title} audio, ${(
+						(downloaded / total) *
+						100
+					).toFixed(2)}%`
+				})
+
+				SoundsData.Add(id, audio).then(writeStream => {
+					writeStream.once('finish', () => {
+						loader.succeed(`Downloaded ${title}`)
+
+						resolve()
+					})
+
+					writeStream.once('error', error => {
+						loader.fail(`Error downloading ${title}`)
+
+						reject(error)
+					})
+				})
+			})
+			.catch(error => {
 				loader.fail(`Error downloading ${title}`)
 
-				reject()
+				reject(error)
 			})
-		})
 	})
 
 export default AddFromYoutube
